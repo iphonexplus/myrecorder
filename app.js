4204074cc6e6c390b72559b460b112125f941528
orange <orange@oranges-MacBook-Pro.local>
2019-08-04 18:40:31 +0800

    test

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var rec; 							//Recorder.js object
 var input; 							//MediaStreamAudioSourceNode we'll be recording
 
 // shim for AudioContext when it's not avb. 
-var AudioContext = window.AudioContext || window.webkitAudioContext;
+// var AudioContext = window.AudioContext;
+var AudioContext = window.webkitAudioContext;
 var audioContext //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
